3b4d7557dea89dcebc8d19975bc88776acd3cc73
karpinskaya <kivvvimail@gmail.com>
2024-03-04 12:22:42 +0300

    init: start graphql task

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLObjectType, GraphQLString, Source, graphql } from 'graphql';
+import { UUIDType } from './types/uuid.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +14,49 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const MemberType = new GraphQLObjectType({
+        name: 'MemberType',
+        fields: () => ({
+          id: { type: GraphQLString },
+          discount: { type: GraphQLString },
+          postsLimitPerMonth: { type: GraphQLString },
+          profiles: { type: GraphQLString },
+        }),
+      });
+
+      const PostType = new GraphQLObjectType({
+        name: 'PostType',
+        fields: () => ({
+          // id: { type: UUIDType },
+        }),
+      });
+
+      const ProfileType = new GraphQLObjectType({
+        name: 'ProfileType',
+        fields: () => ({
+          // id: { type: UUIDType },
+        }),
+      });
+
+      const SubscribersOnAuthorsType = new GraphQLObjectType({
+        name: 'SubscribersOnAuthorsType',
+        fields: () => ({
+          // id: { type: UUIDType },
+        }),
+      });
+
+      const UserType = new GraphQLObjectType({
+        name: 'User',
+        fields: () => ({
+          id: { type: UUIDType },
+        }),
+      });
+
+      const { query, variables } = req.body;
+
+      const source = new Source(query);
+
+      // return await graphql({ schema, source, variableValues: variables });
     },
   });
 };
